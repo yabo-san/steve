4dbd14d32f1c1316cbdadc647afe9c11a7c74b33
Kevin <seniorpepex@gmail.com>
2019-03-30 14:35:17 -0700

    in business

--- myScript.js
+++ myScript.js
@@ -1,9 +1,9 @@
 function randombg(){
-    var random= Math.floor(Math.random() * 10) + 0;
-    var bigSize = ["url('http://placehold.it/300&text=banner1')",
-                   "url('http://placehold.it/300&text=banner2')",
-                   "url('http://placehold.it/300&text=banner3')",
-                   "url('http://placehold.it/300&text=banner4')",
+    var random= Math.floor(Math.random() * 6) + 0;
+    var bigSize = ["url('https://i.redd.it/r0ad288kma8x.jpg')",
+                   "url('https://i.redd.it/al98nvymh16z.png')",
+                   "url('http://i.imgur.com/XjKfgHt.jpg')",
+                   "url('https://www.mobygames.com/images/promo/original/1471982997-3418226446.jpg')",
                    "url('http://placehold.it/300&text=banner5')",
                    "url('http://placehold.it/300&text=banner6')"];
     document.getElementById("random").style.backgroundImage=bigSize[random];
