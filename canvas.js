b895ce5db858de825446186547f768e854653e94
Johnny Urosevic <themagicalcake@gmail.com>
2019-03-30 21:30:56 -0700

    Toggle them boys

--- canvas.js
+++ canvas.js
@@ -13,88 +13,88 @@ function toggle() {
 }    
 
 function drawAnalog() {
-    if (!analog) return;
+    if (analog) {
+        ctx.clearRect(0,0,canvas.width, canvas.height);
+        ctx.beginPath();
+        ctx.arc(canvas.width / 2, canvas.height / 2, 100, 0, 2 * Math.PI, false);
 
-    ctx.clearRect(0,0,canvas.width, canvas.height);
-    ctx.beginPath();
-    ctx.arc(canvas.width / 2, canvas.height / 2, 100, 0, 2 * Math.PI, false);
+        ctx.strokeStyle = "#f3306e";
+        ctx.lineWidth = 10;
+        ctx.stroke();
 
-    ctx.strokeStyle = "#f3306e";
-    ctx.lineWidth = 10;
-    ctx.stroke();
+        clockData();
 
-    clockData();
+        let hour = (hours * 3600 + minutes * 60 + seconds) * 2 * Math.PI / (3600 * 12);
+        let minute = (seconds + minutes * 60) * 2 * Math.PI / (3600);
+        let second = seconds * 2 * Math.PI / 60;
 
-    let hour = (hours * 3600 + minutes * 60 + seconds) * 2 * Math.PI / (3600 * 12);
-    let minute = (seconds + minutes * 60) * 2 * Math.PI / (3600);
-    let second = seconds * 2 * Math.PI / 60;
+        //hour hand
+        ctx.strokeStyle = "#33334d";
+        ctx.translate(canvas.width / 2, canvas.height / 2);
+        ctx.rotate(hour);
+        ctx.translate(-canvas.width / 2, -canvas.height / 2);
 
-    //hour hand
-    ctx.strokeStyle = "#33334d";
-    ctx.translate(canvas.width / 2, canvas.height / 2);
-    ctx.rotate(hour);
-    ctx.translate(-canvas.width / 2, -canvas.height / 2);
+        ctx.beginPath();
+        ctx.lineWidth = 10;
 
-    ctx.beginPath();
-    ctx.lineWidth = 10;
+        ctx.moveTo(canvas.width / 2, canvas.height / 2);
+        ctx.lineTo(canvas.width / 2, canvas.height / 2 - 90);
 
-    ctx.moveTo(canvas.width / 2, canvas.height / 2);
-    ctx.lineTo(canvas.width / 2, canvas.height / 2 - 90);
+        ctx.stroke();
 
-    ctx.stroke();
+        ctx.translate(canvas.width / 2, canvas.height / 2);
+        ctx.rotate(-hour);
+        ctx.translate(-canvas.width / 2, -canvas.height / 2);
 
-    ctx.translate(canvas.width / 2, canvas.height / 2);
-    ctx.rotate(-hour);
-    ctx.translate(-canvas.width / 2, -canvas.height / 2);
-
-    //minute hand
-    ctx.translate(canvas.width / 2, canvas.height / 2);
-    ctx.rotate(minute);
-    ctx.translate(-canvas.width / 2, -canvas.height / 2);
-
-
-    ctx.beginPath();
-    ctx.lineWidth = 6;
-
-    ctx.moveTo(canvas.width / 2, canvas.height / 2);
-    ctx.lineTo(canvas.width / 2, canvas.height / 2 - 90);
-
-    ctx.translate(canvas.width / 2, canvas.height / 2);
-    ctx.rotate(-minute);
-    ctx.translate(-canvas.width / 2, -canvas.height / 2);
-
-    ctx.stroke();
+        //minute hand
+        ctx.translate(canvas.width / 2, canvas.height / 2);
+        ctx.rotate(minute);
+        ctx.translate(-canvas.width / 2, -canvas.height / 2);
 
 
-    //second hand
-    ctx.translate(canvas.width / 2, canvas.height / 2);
-    ctx.rotate(second);
-    ctx.translate(-canvas.width / 2, -canvas.height / 2);
+        ctx.beginPath();
+        ctx.lineWidth = 6;
+
+        ctx.moveTo(canvas.width / 2, canvas.height / 2);
+        ctx.lineTo(canvas.width / 2, canvas.height / 2 - 90);
+
+        ctx.translate(canvas.width / 2, canvas.height / 2);
+        ctx.rotate(-minute);
+        ctx.translate(-canvas.width / 2, -canvas.height / 2);
+
+        ctx.stroke();
 
 
-    ctx.beginPath();
-    ctx.lineWidth = 3;
+        //second hand
+        ctx.translate(canvas.width / 2, canvas.height / 2);
+        ctx.rotate(second);
+        ctx.translate(-canvas.width / 2, -canvas.height / 2);
 
-    ctx.moveTo(canvas.width / 2, canvas.height / 2);
-    ctx.lineTo(canvas.width / 2, canvas.height / 2 - 90);
 
-    ctx.translate(canvas.width / 2, canvas.height / 2);
-    ctx.rotate(-second);
-    ctx.translate(-canvas.width / 2, -canvas.height / 2);
+        ctx.beginPath();
+        ctx.lineWidth = 3;
 
-    ctx.stroke();
+        ctx.moveTo(canvas.width / 2, canvas.height / 2);
+        ctx.lineTo(canvas.width / 2, canvas.height / 2 - 90);
 
+        ctx.translate(canvas.width / 2, canvas.height / 2);
+        ctx.rotate(-second);
+        ctx.translate(-canvas.width / 2, -canvas.height / 2);
+
+        ctx.stroke();
+    }
     requestAnimationFrame(drawAnalog);
 }
 
 function drawDigital(){
-    if (analog) return;
+    if (!analog) {
+        ctx.clearRect(0,0,canvas.width, canvas.height);
 
-    ctx.clearRect(0,0,canvas.width, canvas.height);
+        clockData();
 
-    ctx.font = "30px Arial";
-    ctx.fillText(hours + ":" + minutes + ":" + seconds, 10, 50);
-
+        ctx.font = "30px Arial";
+        ctx.fillText(hours + ":" + minutes + ":" + seconds, 10, 50);
+    }
     requestAnimationFrame(drawDigital);
 }
 
