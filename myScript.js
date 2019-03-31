18f543b94b062361d1a1473563350f63b247efdc
Kevin <seniorpepex@gmail.com>
2019-03-30 20:34:20 -0700

    hi johnny

--- myScript.js
+++ myScript.js
@@ -1,18 +1,26 @@
+var hours
+var minutes
+var seconds
 function randombg(){
-    var random= Math.floor(Math.random() * 6) + 0;
-    var bigSize = ["url('https://timedotcom.files.wordpress.com/2016/11/kanye-west-cancels-saint-pablo-tour.jpg')",
-                   "url('https://i.redd.it/al98nvymh16z.png')",
-                   "url('http://i.imgur.com/XjKfgHt.jpg')",
-                   "url('https://www.mobygames.com/images/promo/original/1471982997-3418226446.jpg')",
-                   "url('https://upload.wikimedia.org/wikipedia/commons/0/01/80_-_Machu_Picchu_-_Juin_2009_-_edit.2.jpg')",
-                   "url('https://www.azamaraclubcruises.com/sites/default/files/heros/rio-de-janeiro-brazil.jpg')"];
-    document.getElementById("random").style.backgroundImage=bigSize[random];
+  var random= Math.floor(Math.random() * 6) + 0;
+  var bigSize = ["url('https://timedotcom.files.wordpress.com/2016/11/kanye-west-cancels-saint-pablo-tour.jpg')",
+                "url('https://i.redd.it/al98nvymh16z.png')",
+                "url('http://i.imgur.com/XjKfgHt.jpg')",
+                "url('https://www.mobygames.com/images/promo/original/1471982997-3418226446.jpg')",
+                "url('https://upload.wikimedia.org/wikipedia/commons/0/01/80_-_Machu_Picchu_-_Juin_2009_-_edit.2.jpg')",
+                "url('https://www.azamaraclubcruises.com/sites/default/files/heros/rio-de-janeiro-brazil.jpg')"];
+  document.getElementById("random").style.backgroundImage=bigSize[random];
 
-  }
-  function ClockData(){
-    var d = new Date();
-    document.getElementById("hour").innerHTML = d.getHours();
-    document.getElementById("minute").innerHTML = d.getMinutes();
-    document.getElementById("second").innerHTML =d.getSeconds();
-  }
+}
+function clockData(){
+  var d = new Date();
+  hours = d.getHours();
+  minutes = d.getMinutes();
+  seconds =d.getSeconds();
+}
+function drawDigital(){
+var c = document.getElementById("clock");
+var ctx = c.getContext("2d");
+
+}
   
