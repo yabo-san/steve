fe11fa8421995431c0d8268f99d8c457d8275088
Johnny Urosevic <themagicalcake@gmail.com>
2019-03-30 20:06:16 -0700

    Added the canvas

---
+++ canvas.js
@@ -0,0 +1,9 @@
+
+var canvas = document.getElementById("clock");
+var ctx = canvas.getContext("2d");
+
+canvas.height = 250;
+canvas.width = 250;
+
+ctx.fillStyle = "#000099";
+ctx.fillRect(0,0,250,250);
